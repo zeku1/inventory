40478a3fa85ef4b91e27b4b86b9a7ee18a9388b1
zeku1 <zekielucena@gmail.com>
2026-07-13 02:02:29 +0800

    Final cleanup and cache sync

--- InventoryERP.Web/obj/Release/net10.0/InventoryERP.Web.AssemblyInfo.cs
+++ InventoryERP.Web/obj/Release/net10.0/InventoryERP.Web.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("InventoryERP.Web")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e1b64f998475ffc0653222e656f74f9d98612e77")]
 [assembly: System.Reflection.AssemblyProductAttribute("InventoryERP.Web")]
 [assembly: System.Reflection.AssemblyTitleAttribute("InventoryERP.Web")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
